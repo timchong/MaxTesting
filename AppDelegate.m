29319fffabf6b9ca199a039ba67e7837fd637235
timchong <timchong@outlook.com>
2013-07-16 16:52:44 +0800

    branch fix

--- AppDelegate.m
+++ AppDelegate.m
@@ -36,7 +36,7 @@
     _window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];  
     self.window.backgroundColor = [UIColor whiteColor];
     self.window.rootViewController = self.navigationController;
-    [self.window makeKeyAndVisible];
+    [self.window makeKeyAndVisible]; //From branch
     return YES;
 }
 
